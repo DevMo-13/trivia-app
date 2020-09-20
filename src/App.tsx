612941c26f79349b04946ea4ce45a10805b7ba38
DevMo-13 <moniquecinatl@gmail.com>
2020-09-20 16:04:00 -0700

    Set initial css variables

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 
-import './styles/App.scss';
-
 function App() {
 	return (
-		<div className='App'>
-			<header className='App-header'>
-				<h1>Hello World</h1>
+		<div className='app'>
+			<header className='app-header'>
+				<h1 className='hello'>Hello World</h1>
 				<p>Sass-y</p>
 				<button>Practice</button>
 			</header>
